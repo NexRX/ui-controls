4a6925fc5d2e15c150adc1936a4719ca50017714
Callum Cunha <callum.cunha@os.uk>
2024-08-10 18:15:04 +0100

    FFT testing

--- src/matching/cpu.rs
+++ src/matching/cpu.rs
@@ -1,126 +1,208 @@
 use super::SearchResult;
 use image::{flat::SampleLayout, DynamicImage, GrayImage, ImageBuffer, Luma};
 use ndarray::{s, Array2, ArrayView2, ShapeBuilder};
+use ndrustfft::*;
 use rayon::iter::{ParallelBridge, ParallelIterator};
+use std::path::Path;
 
-type LocatedNCC = (f32, (usize, usize));
+// TODO: Comething seems wrong with how we are iterating, things stick to the left especiallly with template. 
+// Check it out, maybe we need to do something about it, we willl see
 
+fn load_image_to_complex_array(file_path: &str) -> Array2<Complex<f32>> {
+    let img = image::open(file_path).expect("Failed to open image");
+    let gray_img = img.to_luma32f();
+    let (width, height) = gray_img.dimensions();
+    let mut complex_array = Array2::<Complex<f32>>::zeros((width as usize, height as usize));
 
-// TODO: Move to this for speed ups https://docs.rs/ndrustfft/latest/ndrustfft/
-
-// Helper function to convert image to ndarray array
-fn image_to_ndarray(image: ImageBuffer<Luma<f32>, Vec<f32>>) -> Array2<f32> {
-    let SampleLayout {
-        height,
-        height_stride,
-        width,
-        width_stride,
-        ..
-    } = image.sample_layout();
-    let shape = (width as usize, height as usize);
-    let strides = (width_stride, height_stride);
-    Array2::from_shape_vec(shape.strides(strides), image.into_raw()).unwrap()
-}
-
-fn compute_region_ncc(
-    image: &ArrayView2<f32>,
-    template: &ArrayView2<f32>,
-    template_mean: f32,
-    template_std: f32,
-    (x, y): (usize, usize),
-) -> LocatedNCC {
-    let (tw, th) = template.dim();
-    let region = image.slice(s![x..x + tw, y..y + th]);
-
-    // Convert the region to f32 and compute the mean and standard deviation
-    let region_mean = region.mean().unwrap();
-    let region_std = region
-        .mapv(|x| (x - region_mean).powi(2))
-        .mean()
-        .unwrap()
-        .sqrt();
-
-    // Compute the NCC score
-    let numerator = ((region.mapv(|v| v - region_mean)) * (template - template_mean)).sum();
-    let denominator = region_std * template_std * template.len() as f32;
-
-    let ncc = if denominator != 0.0 {
-        numerator / denominator
-    } else {
-        0.0
-    };
-
-    (ncc, (x, y))
-}
-
-fn compute_standard_deviation(image: &ArrayView2<f32>, mean: f32) -> f32 {
-    (image - mean).mapv(|x| x.powi(2)).mean().unwrap().sqrt()
-}
-
-fn max_ncc(a: LocatedNCC, b: LocatedNCC) -> LocatedNCC {
-    if a.0 > b.0 {
-        return a;
+    for (x, y, luma) in gray_img.enumerate_pixels() {
+        let pixel_value = luma[0];
+        complex_array[(x as usize, y as usize)] = Complex::new(pixel_value, 0.0);
     }
-    b
+
+    complex_array
 }
 
-fn normalize_cross_correlation(
-    image: &ArrayView2<f32>,
-    template: &ArrayView2<f32>,
-) -> (f32, (usize, usize)) {
-    let (iw, ih) = image.dim();
+// Pad array to a specific size
+fn pad_to_size(array: &Array2<Complex<f32>>, new_size: (usize, usize)) -> Array2<Complex<f32>> {
+    let (w, h) = array.dim();
+    let (new_w, new_h) = new_size;
+
+    let mut padded = Array2::<Complex<f32>>::zeros((new_w, new_h));
+
+    for x in 0..w {
+        for y in 0..h {
+            padded[(x, y)] = array[(x, y)];
+        }
+    }
+
+    padded
+}
+
+// Perform 2D FFT
+fn fft2d(data: &Array2<f32>) -> Array2<Complex<f32>> {
+    let (w, h) = data.dim();
+    let mut output = Array2::<Complex<f32>>::zeros(((w / 2) + 1, h));
+    let planner = R2cFftHandler::<f32>::new(w);
+
+    ndfft_r2c_par(data, &mut output, &planner, 0);
+
+    output
+}
+
+// Perform 2D IFFT
+fn ifft2d(data: &Array2<Complex<f32>>, original_dim: Option<(usize, usize)>) -> Array2<f32> {
+    let (w, h) = data.dim();
+
+    // Adjust the dimensions to match the original image size (before padding)
+    let mut output = Array2::<f32>::zeros(original_dim.unwrap_or((w * 2 - 2, h * 2 - 2)));
+
+    let planner = R2cFftHandler::<f32>::new(output.dim().0);
+    // r2c actually means complex to real since its ifft
+    ndifft_r2c_par(data, &mut output, &planner, 0);
+
+    output
+}
+
+// Perform cross-correlation
+fn cross_correlation(
+    target: &Array2<Complex<f32>>,
+    template: &Array2<Complex<f32>>,
+) -> Array2<f32> {
     let (tw, th) = template.dim();
-    assert!(ih > th && iw > tw, "Template dimensions supercede images");
+    let (w, h) = target.dim();
 
-    let template_mean = template.mean().unwrap();
-    let template_std = compute_standard_deviation(template, template_mean);
+    // Pad template to match the size of the target
+    let padded_template = pad_to_size(template, (w, h));
 
-    let slide_range = s![0..(iw - tw), 0..(ih - th)];
-    let image_slide_bounds = image.slice(slide_range);
+    // Compute FFT of target and template
 
-    let best = image_slide_bounds
-        .indexed_iter()
-        .par_bridge()
-        .map(|((x, y), _px)| {
-            compute_region_ncc(image, &template, template_mean, template_std, (x, y))
-        })
-        .reduce(|| (0.0, (0, 0)), max_ncc);
+    let target_fft = fft2d(&Array2::<f32>::from(target.mapv(|c| c.re)));
+    save_arrayc(&target_fft, "target/target_ftt.png");
+    let template_fft = fft2d(&Array2::<f32>::from(padded_template.mapv(|c| c.re)));
+    save_arrayc(&template_fft, "target/template_fft.png");
 
+    // Compute cross-correlation in frequency domain
+    let template_fft_conj = template_fft.mapv(|c| c.conj());
+    let mut result_fft = target_fft * template_fft_conj;
+    save_arrayc(&result_fft, "target/result_fft.png");
+    
+    // Perform IFFT to obtain the cross-correlation result
+    let result_ifft = ifft2d(&result_fft, Some(target.dim()));
+    save_array(&result_ifft, "target/result_ifft.png");
 
-    let best_percentage = (best.0 * 100.0).clamp(0.0, 100.0);
-
-    (best_percentage, best.1)
+    result_ifft
 }
-pub fn find_target(
-    image: &DynamicImage,
-    template: &DynamicImage,
-    confidence_threshold: f32,
-) -> SearchResult {
-    let result = normalize_cross_correlation(
-        &image_to_ndarray(image.to_luma32f()).view(),
-        &image_to_ndarray(template.to_luma32f()).view(),
-    );
 
-    Ok(Some((result.1 .0 as i32, result.1 .1 as i32)))
+// Find the position of the template in the target image
+fn find_template_position(correlation: &Array2<f32>) -> (usize, usize) {
+    let (w, h) = correlation.dim();
+    let mut max_val = f32::MIN;
+    let mut max_pos = (0, 0);
+
+    for ((x, y), &val) in correlation.indexed_iter() {
+        if val > max_val {
+            max_val = val;
+            max_pos = (x, y);
+        }
+    }
+
+    max_pos
+}
+
+fn compute_template_position(
+    target: &Array2<Complex<f32>>,
+    template: &Array2<Complex<f32>>,
+) -> (usize, usize) {
+    let correlation = cross_correlation(target, template);
+    save_array(&correlation, "target/correlation.png");
+    find_template_position(&correlation)
+}
+
+fn image_to_ndarray(image: ImageBuffer<Luma<f32>, Vec<f32>>) -> Array2<f32> {
+    // Create an ndarray from the image data
+    let ndarray = Array2::<f32>::from_shape_vec(
+        (image.height() as usize, image.width() as usize),
+        image.into_raw(),
+    )
+    .expect("Failed to create ndarray from image data");
+
+    ndarray
+}
+
+fn open_image_array(path: &str) -> Array2<f32> {
+    let image = image::open(path).expect("Failed to open image");
+    image_to_ndarray(image.to_luma32f())
+}
+
+fn array_to_image(ndarray: &Array2<f32>) -> ImageBuffer<Luma<f32>, Vec<f32>> {
+    let (height, width) = ndarray.dim();
+
+    // Create an ImageBuffer with the same dimensions as the ndarray
+    let mut img = ImageBuffer::<Luma<f32>, Vec<f32>>::new(width as u32, height as u32);
+
+    // Fill the image buffer with the ndarray data
+    for y in 0..height {
+        for x in 0..width {
+            img.put_pixel(x as u32, y as u32, Luma::from([ndarray[[y, x]]]));
+        }
+    }
+
+    img
+}
+
+fn save_array(ndarray: &Array2<f32>, path: &str) {
+    let image = array_to_image(ndarray);
+    let (width, height) = image.dimensions();
+    
+    // Create a new image buffer to hold the u8 data
+    let mut image_u8 = ImageBuffer::new(width, height);
+
+    // Iterate over each pixel
+    for (x, y, pixel_f32) in image.enumerate_pixels() {
+        // Convert the f32 value to u8
+        let Luma([value_f32]) = *pixel_f32;
+        let value_u8 = (value_f32 * 255.0).clamp(0.0, 255.0) as u8;
+        
+        // Set the pixel value in the new image buffer
+        image_u8.put_pixel(x, y, Luma([value_u8]));
+    }
+    image_u8.save(path).expect("Failed to save image");
+}
+
+fn save_arrayc(ndarray: &Array2<Complex<f32>>, path: &str) {
+   save_array(&ndarray.map(|v| v.re), path);
 }
 
 #[cfg(test)]
-mod test_target {
+mod test {
+    use approx::assert_relative_eq;
+
     use super::*;
-    use image::GrayImage;
 
     #[test]
     fn finds_button_at_high_confidence() {
-        use approx::assert_relative_eq;
+        let source = open_image_array("__fixtures__/screen-2k.png").map(|&x| Complex::new(x, 0.0));
+        save_array(&source.mapv(|v| v.re), "target/source.png");
+        let target = open_image_array("__fixtures__/btn.png").map(|&x| Complex::new(x, 0.0));
+        save_array(&target.mapv(|v| v.re), "target/target.png");
 
-        let source = image::open("__fixtures__/screen-2k.png").unwrap();
-        let target = image::open("__fixtures__/btn.png").unwrap();
-
-        let (x, y) = super::find_target(&source, &target, 0.99999)
-            .expect("An error occured")
-            .expect("No cordinates found");
+        let (x,y) = compute_template_position(&source, &target);
 
         assert_relative_eq!(x as f32, 1180f32, max_relative = 0.0017);
         assert_relative_eq!(y as f32, 934f32, max_relative = 0.0017);
     }
+    // #[test]
+    // fn finds_button_at_high_confidence() {
+    //     use approx::assert_relative_eq;
+
+    //     let source = image::open("__fixtures__/screen-2k.png").unwrap();
+    //     let target = image::open("__fixtures__/btn.png").unwrap();
+
+    //     let (x, y) = super::find_target(&source, &target, 0.99999)
+    //         .expect("An error occured")
+    //         .expect("No cordinates found");
+
+    //     assert_relative_eq!(x as f32, 1180f32, max_relative = 0.0017);
+    //     assert_relative_eq!(y as f32, 934f32, max_relative = 0.0017);
+    // }
 }
