e1304cb50d4c30f26872daa4a6e9c0e0586c0054
Nex <callum.cunha@gmail.com>
2024-08-23 01:09:12 +0100

    revert cpu changes to last working version manually

--- src/matching/cpu.rs
+++ src/matching/cpu.rs
@@ -1,10 +1,9 @@
-use crate::types::matrix::{Matrix, MatrixImage, MatrixImageC};
 use image::DynamicImage;
-use rayon::iter::{
-    IndexedParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator,
-};
+use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator};
 use rustfft::{num_complex::Complex, num_traits::Zero, FftPlanner};
 use std::vec;
+
+use crate::types::matrix::Matrix;
 
 // TODO: Clean up
 // TODO: Implement alternative to NCC using Sum of Squared Differences (SSD)
@@ -17,16 +16,20 @@ use std::vec;
 //                    \/-----------\/
 //and you can expect a 2x-5x speedup. If that’s still too slow, find a nearby size whose prime factors are all 2 or 3, and you can expect a 1.1x-1.5x speedup.
 
-fn fft2d(mut image: MatrixImageC) -> MatrixImageC {
+fn fft2d(mut image: Matrix<Complex<f64>>) -> Matrix<Complex<f64>> {
     let (height, width) = image.dims();
 
     let mut planner = FftPlanner::new();
     let fft_row = planner.plan_fft_forward(width);
     let fft_col = planner.plan_fft_forward(height);
-
+    
+    // Apply FFT to each row in parallel
+    image.par_iter_rows_mut().for_each(|row| {
+        fft_row.process(row);
+    });
+    
     // Transpose the image for column FFTs (rows/height and cols/width are flipped for easier and better processing)
-    let mut transposed_image: MatrixImageC = Matrix::new_zeros(width, height);
-    assert_eq!(image.dims(), (transposed_image.cols(), transposed_image.rows()));
+    let mut transposed_image: Matrix<Complex<f64>> = Matrix::new_zeros(width, height);
 
     // Transpose the image so that columns become rows
     transposed_image
@@ -37,42 +40,33 @@ fn fft2d(mut image: MatrixImageC) -> MatrixImageC {
             }
         });
 
-    // Apply FFT to each row in parallel
-    let fn_row = move || {
-        image.par_iter_rows_mut().for_each(|row| {
-            fft_row.process(row);
-        });
-        image
-    };
-
-    let fn_col = || {
-        // Apply FFT to each transposed row (original column) in parallel
-        transposed_image.par_iter_rows_mut().for_each(|row| {
-            fft_col.process(row);
-        });
-
-        transposed_image
-    };
-
-    let (mut image, transposed_image) = rayon::join(fn_row, fn_col);
+    // Apply FFT to each transposed row (original column) in parallel
+    transposed_image.par_iter_rows_mut().for_each(|row| {
+        fft_col.process(row);
+    });
 
     // Transpose back to the original layout
-    image.par_enumerate_element_mut().for_each(|((row, col), val)| {
-            assert!(row < transposed_image.cols(), "Expected {row} to be less than {}", transposed_image.rows());
-            assert!(col < transposed_image.rows(), "Expected {col} to be less than {}", transposed_image.rows());
-            *val = transposed_image[(col, row)];
+    image.par_enumerate_rows_mut().for_each(|(i, row)| {
+        for (col, val) in row.iter_mut().enumerate() {
+            *val = transposed_image[(col, i)];
+        }
     });
 
     image
 }
 
 /// Computes the inverse 2D FFT of a 2D Vec.
-fn ifft2d(mut frequency_domain: MatrixImageC) -> MatrixImageC {
+fn ifft2d(mut frequency_domain: Matrix<Complex<f64>>) -> Matrix<Complex<f64>> {
     let (height, width) = frequency_domain.dims();
 
     let mut planner = FftPlanner::new();
     let ifft_row = planner.plan_fft_inverse(width);
     let ifft_col = planner.plan_fft_inverse(height);
+
+    // Apply inverse FFT to each row in parallel
+    frequency_domain.par_iter_rows_mut().for_each(|row| {
+        ifft_row.process(row);
+    });
 
     // Transpose the result for column FFTs
     let mut transposed_result: Vec<Vec<Complex<f64>>> = vec![vec![Complex::zero(); height]; width];
@@ -86,23 +80,10 @@ fn ifft2d(mut frequency_domain: MatrixImageC) -> MatrixImageC {
             }
         });
 
-    // Apply inverse FFT to each row in parallel
-    let fn_row = || {
-        frequency_domain.par_iter_rows_mut().for_each(|row| {
-            ifft_row.process(row);
-        });
-        frequency_domain
-    };
-
     // Apply inverse FFT to each transposed row (original column) in parallel
-    let fn_col = || {
-        transposed_result.par_iter_mut().for_each(|row| {
-            ifft_col.process(row);
-        });
-        transposed_result
-    };
-
-    let (mut frequency_domain, transposed_result) = rayon::join(fn_row, fn_col);
+    transposed_result.par_iter_mut().for_each(|row| {
+        ifft_col.process(row);
+    });
 
     // Transpose back to the original layout
     frequency_domain
@@ -124,7 +105,27 @@ fn ifft2d(mut frequency_domain: MatrixImageC) -> MatrixImageC {
     frequency_domain
 }
 
-fn compute_cross_spectrum(ga: &MatrixImageC, gb: &MatrixImageC) -> MatrixImageC {
+/// Convert an image to a 2D vector of Complex numbers (grayscale values).
+fn image_to_complex_matrix(image: &DynamicImage) -> Matrix<Complex<f64>> {
+    let image = image.to_luma8();
+    let (width, height) = image.dimensions();
+
+    let mut result = Matrix::<Complex<f64>>::new_zeros(height as usize, width as usize);
+
+    for y in 0..height {
+        for x in 0..width {
+            let pixel = image.get_pixel(x, y).0[0] as f64;
+            result[(y as usize, x as usize)] = Complex::new(pixel, 0.0);
+        }
+    }
+
+    result
+}
+
+fn compute_cross_spectrum(
+    ga: &Matrix<Complex<f64>>,
+    gb: &Matrix<Complex<f64>>,
+) -> Matrix<Complex<f64>> {
     let (height, width) = ga.dims();
     let mut result = Matrix::new_zeros(height, width);
 
@@ -153,7 +154,10 @@ fn max_fft(a: (f64, (usize, usize)), b: (f64, (usize, usize))) -> (f64, (usize, 
 }
 
 // Function to find the peak correlation
-fn find_peak_correlation(correlation: &MatrixImageC, normalized: bool) -> (f64, (usize, usize)) {
+fn find_peak_correlation(
+    correlation: &Matrix<Complex<f64>>,
+    normalized: bool,
+) -> (f64, (usize, usize)) {
     let (height, width) = correlation.dims();
 
     let peak = (0..height)
@@ -192,7 +196,7 @@ fn find_peak_correlation(correlation: &MatrixImageC, normalized: bool) -> (f64, 
     (peak_norm, peak.1)
 }
 
-fn pad_image(image: &MatrixImageC, width: usize, height: usize) -> MatrixImageC {
+fn pad_image(image: &Matrix<Complex<f64>>, width: usize, height: usize) -> Matrix<Complex<f64>> {
     let mut padded = Matrix::new_zeros(height, width);
     let (og_height, og_width) = image.dims();
 
@@ -209,8 +213,8 @@ pub fn template_matching(source: &DynamicImage, template: &DynamicImage) -> (f64
     let (w, h) = (source.width() as usize, source.height() as usize);
 
     // Pad the template image to match the background dimensions
-    let source = MatrixImage::from(source).into_complex();
-    let template = pad_image(&MatrixImage::from(template).into_complex(), w, h);
+    let source = image_to_complex_matrix(source);
+    let template = pad_image(&image_to_complex_matrix(template), w, h);
 
     let fn_ga = || fft2d(source);
     let fn_gb = || fft2d(template);
